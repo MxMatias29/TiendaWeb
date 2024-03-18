899f130991d378e3d826c6446b94e31c442fcdc6
Matias Mayorga <pt74882334@idat.edu.pe>
2024-03-18 15:41:39 -0500

    Add CORS configuration and update database properties***

--- tienda/src/main/java/com/example/tienda/TiendaApplication.java
+++ tienda/src/main/java/com/example/tienda/TiendaApplication.java
@@ -2,6 +2,9 @@ package com.example.tienda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TiendaApplication {
@@ -9,6 +12,17 @@ public class TiendaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaApplication.class, args);
 		System.out.println("Is running...");
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH");
+
+			}
+		};
 	}
 
 }
